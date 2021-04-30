50d44d645cf4019e341390ebeacf7ce603783e99
James Fisher <jfisher396@hotmail.com>
2021-04-30 13:49:58 -0700

    server file created

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const allRoutes = require("./controllers");
+
+const db = require('./models');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use('/', allRoutes);
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+        console.log(`App listening on ${PORT}`)
+    })
+})
