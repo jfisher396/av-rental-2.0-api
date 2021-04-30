8153519a3b59d12a3dec4ea3e800fc1e644cc3e0
James Fisher <jfisher396@hotmail.com>
2021-04-30 13:55:35 -0700

    server functional

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const allRoutes = require("./controllers");
 
-const db = require('./models');
+const db = require("./models");
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', allRoutes);
+app.use("/", allRoutes);
 
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function() {
-        console.log(`App listening on ${PORT}`)
-    })
-})
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, () => console.log(`App listening on ${PORT}`));
+});
