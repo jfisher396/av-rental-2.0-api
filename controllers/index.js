32a3f4ed441f7424df944f3d0b862c6578667cb8
James Fisher <jfisher396@hotmail.com>
2021-05-01 13:58:12 -0700

    equipment route files created

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+const projectorRoutes = require("./projector");
+const screenRoutes = require("./screen");
+const computerRoutes = require("./computer");
+const audioRoutes = require("./audio");
+const miscRoutes = require("./misc");
+
 router.get("/", (req, res) => {
   res.send("Here fishy fishy.");
 });
+
+router.use("/api/projector", projectorRoutes);
+router.use("/api/screen", screenRoutes);
+router.use("/api/computer", computerRoutes);
+router.use("/api/audio", audioRoutes);
+router.use("/api/misc", miscRoutes)
 
 module.exports = router;
