e40a6f12aab551a15a019f3c75885a6d0ed06d83
James Fisher <jfisher396@hotmail.com>
2021-05-05 11:26:32 -0700

    computers model updated

--- models/Computer.js
+++ models/Computer.js
@@ -1,11 +1,16 @@
-module.exports = function(sequelize, DataTypes) {
-    var Computer = sequelize.define('Computer', {
-        type:DataTypes.STRING,
-        model:DataTypes.STRING,
-        price:DataTypes.INTEGER,
-        image:DataTypes.STRING,
-        description:DataTypes.STRING
-    })
+module.exports = function (sequelize, DataTypes) {
+  var Computer = sequelize.define("Computer", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: "true",
+    },
+    brand: DataTypes.STRING,
+    model: DataTypes.STRING,
+    screenSize: DataTypes.STRING,
+    rate: DataTypes.INTEGER,
+    image: DataTypes.STRING,
+    description: DataTypes.STRING,
+  });
 
-    return Computer
-}
+  return Computer;
+};
--- models/Screen.js
+++ models/Screen.js
@@ -1,15 +1,15 @@
-module.exports = function(sequelize, DataTypes) {
-    var Screen = sequelize.define('Screen', {
-        id: { 
-            type: DataTypes.INTEGER,
-            primaryKey: "true"
-        },
-        model:DataTypes.STRING,
-        size: DataTypes.STRING,
-        price:DataTypes.INTEGER,
-        image:DataTypes.STRING,
-        description:DataTypes.STRING
-    })
+module.exports = function (sequelize, DataTypes) {
+  var Screen = sequelize.define("Screen", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: "true",
+    },
+    model: DataTypes.STRING,
+    size: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    image: DataTypes.STRING,
+    description: DataTypes.STRING,
+  });
 
-    return Screen
-}
+  return Screen;
+};
