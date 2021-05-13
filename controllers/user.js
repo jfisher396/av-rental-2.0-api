0b799ac22a7717533f8b43e8fa4ead9f0b63a5d3
James Fisher <jfisher396@hotmail.com>
2021-05-13 16:41:04 -0700

    password bcrypted

--- controllers/user.js
+++ controllers/user.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models")
+const db = require("../models");
+const bcrypt = require("bcrypt");
 
 router.get("/", (req, res) => {
-  db.User.findAll().then(user => {
-    res.json(user)
-  }).catch(err => {
-    console.log(err);
-    res.status(500).end()
-  })
+  db.User.findAll()
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
 });
 
-router.post("/", (req,res) => {
+router.post("/signup", (req, res) => {
   db.User.create({
     email: req.body.email,
-    password: req.body.password
-  }).then(newUser => {
-    res.json(newUser)
-  }).catch(err => {
-    console.log(err);
-    res.status(500).end()
+    password: req.body.password,
   })
-})
+    .then((newUser) => {
+      res.json(newUser);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
 
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,12 +1,17 @@
+const bcrypt = require("bcrypt")
+
 module.exports = function (sequelize, DataTypes) {
   const User = sequelize.define("User", {
-    // id: {
-    //   type:DataTypes.INTEGER,
-    //   primaryKey: "true",
-    // },
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
     password: DataTypes.STRING,
   });
+
+  User.beforeCreate(function (user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
 
   return User;
 };
