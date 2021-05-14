13c3a9410a837433704e823a8aa7bec59303d46b
James Fisher <jfisher396@hotmail.com>
2021-05-14 10:25:07 -0700

    login function working

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(helmet())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000"],
   credentials: true
 }))
 app.use(session({
