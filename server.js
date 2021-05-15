84eae1a6effe71471a35534e9309e0acdf65cac7
James Fisher <jfisher396@hotmail.com>
2021-05-15 09:09:06 -0700

    added deployed config and added secret code to env

--- server.js
+++ server.js
@@ -14,18 +14,36 @@ const PORT = process.env.PORT || 8080
 app.use(helmet())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+//local config
 app.use(cors({
   origin: ["http://localhost:3000"],
   credentials: true
 }))
 app.use(session({
-  secret: "secretcode",
+  secret: process.env.SECRET_CODE,
   resave: false,
   saveUninitialized: false,
   cookie: {
     maxAge: 2*60*60*1000
   }
 }))
+
+//deployed config
+// app.use(cors({
+//   origin: ["https://av-rentals-2.herokuapp.com/"],
+//   credentials: true
+// }))
+// app.use(session({
+//   secret: "secretcode",
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//     maxAge: 2*60*60*1000,
+//     sameSite: "none",
+//     secure: true
+//   }
+// }))
 
 //Routes
 app.use("/", allRoutes)
